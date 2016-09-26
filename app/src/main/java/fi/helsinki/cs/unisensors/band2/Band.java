4b173f7e5ca36ede3bccf29b5e5327279cb18c66
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2016-09-26 14:51:45 +0300

    Generic band class for registering sensors

---
+++ app/src/main/java/fi/helsinki/cs/unisensors/band2/Band.java
@@ -0,0 +1,162 @@
+package fi.helsinki.cs.unisensors.band2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.UserConsent;
+import com.microsoft.band.sensors.BandGsrEventListener;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.GsrSampleRate;
+import com.microsoft.band.sensors.HeartRateConsentListener;
+
+import java.lang.ref.WeakReference;
+
+public class Band {
+    private static String TAG = Band.class.getSimpleName();
+    private static BandClient client;
+    private final static String ERR_NOT_CONNECTED = "Band not connected or within reach of bluetooth!";
+
+    public static void registerGsrListener(Context context, BandGsrEventListener listener){
+        new GsrSubscriptionTask(context, listener).execute();
+    }
+
+    public static void registerHrListener(Context context, BandHeartRateEventListener listener){
+        new HrSubscriptionTask(context, listener).execute();
+    }
+
+    private static void logBandException(BandException e){
+        String exceptionMessage;
+        switch (e.getErrorType()) {
+            case UNSUPPORTED_SDK_VERSION_ERROR:
+                exceptionMessage = "Unsupported SDK version, please update.";
+                break;
+            case SERVICE_ERROR:
+                exceptionMessage = "Incorrect permissions or missing Microsoft Band application.";
+                break;
+            default:
+                exceptionMessage = "Unknown band error: " + e.getMessage();
+                break;
+        }
+        Log.e(TAG, exceptionMessage);
+    }
+
+    private static boolean getConnectedBandClient(Context context) throws InterruptedException, BandException {
+        if (client == null) {
+            BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+            if (devices.length == 0) {
+                Log.d(TAG, "Band isn't paired!");
+                return false;
+            }
+            client = BandClientManager.getInstance().create(context, devices[0]);
+        } else if (ConnectionState.CONNECTED == client.getConnectionState()) {
+            Log.d(TAG, "Already connected to band!");
+            return true;
+        }
+        Log.d(TAG, "Connecting to band..");
+        return ConnectionState.CONNECTED == client.connect().await();
+    }
+
+    private static class GsrSubscriptionTask extends AsyncTask<Void, Void, Void> {
+        private Context context;
+        private BandGsrEventListener listener;
+
+        GsrSubscriptionTask(Context context, BandGsrEventListener listener){
+            this.context = context;
+            this.listener = listener;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient(context)) {
+                    int hardwareVersion = Integer.parseInt(client.getHardwareVersion().await());
+                    if (hardwareVersion >= 20) {
+                        Log.d(TAG, "Successfully connected!");
+                        client.getSensorManager().registerGsrEventListener(listener, GsrSampleRate.MS200);
+                    } else {
+                        Log.e(TAG, "GSR cannot be measured on Band 1");
+                    }
+                } else {
+                    Log.e(TAG, ERR_NOT_CONNECTED);
+                }
+            } catch (BandException e) {
+                logBandException(e);
+            } catch (Exception e) {
+                Log.e(TAG, "Generic error when subscribing to GSR: " + e.getMessage());
+            }
+            return null;
+        }
+    }
+
+    private static class HrSubscriptionTask extends AsyncTask<Void, Void, Void> {
+        private Context context;
+        private BandHeartRateEventListener listener;
+
+        HrSubscriptionTask(Context context, BandHeartRateEventListener listener){
+            this.context = context;
+            this.listener = listener;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient(context)) {
+                    if (client.getSensorManager().getCurrentHeartRateConsent() == UserConsent.GRANTED) {
+                        client.getSensorManager().registerHeartRateEventListener(listener);
+                    } else {
+                        Log.e(TAG, "Heart rate permission missing!");
+                    }
+                } else {
+                    Log.e(TAG, ERR_NOT_CONNECTED);
+                }
+            } catch (BandException e) {
+                logBandException(e);
+
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+            return null;
+        }
+    }
+
+    private static class HrConsentTask extends AsyncTask<WeakReference<Activity>, Void, Void> {
+        private Context context;
+        private BandGsrEventListener listener;
+
+        HrConsentTask(Context context, BandGsrEventListener listener){
+            this.context = context;
+            this.listener = listener;
+        }
+
+        @SafeVarargs
+        @Override
+        protected final Void doInBackground(WeakReference<Activity>... params) {
+            try {
+                if (getConnectedBandClient(context)) {
+                    if (params[0].get() != null) {
+                        client.getSensorManager().requestHeartRateConsent(params[0].get(), new HeartRateConsentListener() {
+                            @Override
+                            public void userAccepted(boolean consentGiven) {
+                                Log.d(TAG, "Consent given for Heart Rate!");
+                            }
+                        });
+                    }
+                } else {
+                    Log.e(TAG, ERR_NOT_CONNECTED);
+                }
+            } catch (BandException e) {
+                logBandException(e);
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+            return null;
+        }
+    }
+}
--- app/src/main/java/fi/helsinki/cs/unisensors/band2/BandService.java
+++ app/src/main/java/fi/helsinki/cs/unisensors/band2/BandService.java
@@ -6,32 +6,38 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.util.Log;
 
 import com.microsoft.band.BandClient;
-import com.microsoft.band.BandClientManager;
-import com.microsoft.band.BandException;
-import com.microsoft.band.BandInfo;
-import com.microsoft.band.ConnectionState;
 import com.microsoft.band.sensors.BandGsrEvent;
 import com.microsoft.band.sensors.BandGsrEventListener;
-import com.microsoft.band.sensors.GsrSampleRate;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
 
 public class BandService extends Service {
     private final String TAG = this.getClass().getSimpleName();
     private final int ID = 42;
     private NotificationManager mNotificationManager;
-    private BandClient client = null;
+    private int skinResponse, heartRate;
+
+
     private BandGsrEventListener gsrListener = new BandGsrEventListener() {
         @Override
         public void onBandGsrChanged(final BandGsrEvent event) {
             if (event != null) {
-                updateNotification("GSR: " + event.getResistance() + "kOhm");
+                skinResponse = event.getResistance();
+                updateNotification();
+            }
+        }
+    };
+    private BandHeartRateEventListener hrListener = new BandHeartRateEventListener() {
+        @Override
+        public void onBandHeartRateChanged(final BandHeartRateEvent event) {
+            if (event != null) {
+                heartRate = event.getHeartRate();
+                updateNotification();
             }
         }
     };
@@ -58,65 +64,16 @@ public class BandService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         startForeground(ID, getPersistentServiceNotification("Initializing.."));
         mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        new GsrSubscriptionTask().execute();
+
+        Context baseContext = getBaseContext();
+        Band.registerGsrListener(baseContext, gsrListener);
+        Band.registerHrListener(baseContext, hrListener);
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void updateNotification(String status){
+    private void updateNotification(){
+        String status = "GSR: " + skinResponse + " kOhm, HR: " + heartRate;
         mNotificationManager.notify(ID, getPersistentServiceNotification(status));
     }
 
-
-    private boolean getConnectedBandClient() throws InterruptedException, BandException {
-        if (client == null) {
-            BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
-            if (devices.length == 0) {
-                Log.d(TAG, "Band isn't paired!");
-                return false;
-            }
-            client = BandClientManager.getInstance().create(getBaseContext(), devices[0]);
-        } else if (ConnectionState.CONNECTED == client.getConnectionState()) {
-            Log.d(TAG, "Already connected to band!");
-            return true;
-        }
-        Log.d(TAG, "Connecting to band..");
-        return ConnectionState.CONNECTED == client.connect().await();
-    }
-
-    private class GsrSubscriptionTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                if (getConnectedBandClient()) {
-                    int hardwareVersion = Integer.parseInt(client.getHardwareVersion().await());
-                    if (hardwareVersion >= 20) {
-                        Log.d(TAG, "Successfully connected!");
-                        client.getSensorManager().registerGsrEventListener(gsrListener, GsrSampleRate.MS200);
-                    } else {
-                        Log.e(TAG, "GSR cannot be measured on Band 1");
-                    }
-                } else {
-                    Log.e(TAG, "Band not connected or within reach of bluetooth!");
-                }
-            } catch (BandException e) {
-                String exceptionMessage;
-                switch (e.getErrorType()) {
-                    case UNSUPPORTED_SDK_VERSION_ERROR:
-                        exceptionMessage = "Unsupported SDK version, please update.";
-                        break;
-                    case SERVICE_ERROR:
-                        exceptionMessage = "Incorrect permissions or missing Microsoft Band application.";
-                        break;
-                    default:
-                        exceptionMessage = "Unknown band error: " + e.getMessage();
-                        break;
-                }
-                Log.e(TAG, exceptionMessage);
-
-            } catch (Exception e) {
-                Log.e(TAG, "Generic error when subscribing to GSR: " + e.getMessage());
-            }
-            return null;
-        }
-    }
 }
